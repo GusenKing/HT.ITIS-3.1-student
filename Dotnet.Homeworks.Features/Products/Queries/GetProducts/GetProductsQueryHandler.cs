56582ba74355dffa0581886eecb37d401c632ad7
Emin <emin.sarux2004@yandex.ru>
2025-02-02 17:10:01 +0300

    hw 6

--- Dotnet.Homeworks.Features/Products/Queries/GetProducts/GetProductsQueryHandler.cs
+++ Dotnet.Homeworks.Features/Products/Queries/GetProducts/GetProductsQueryHandler.cs
@@ -1,5 +1,22 @@
+using Dotnet.Homeworks.Infrastructure.Cqrs.Queries;
+using Dotnet.Homeworks.Infrastructure.UnitOfWork;
+using Dotnet.Homeworks.Shared.Dto;
+
 namespace Dotnet.Homeworks.Features.Products.Queries.GetProducts;
 
-internal sealed class GetProductsQueryHandler //TODO: Inherit certain interface 
+internal sealed class GetProductsQueryHandler : IQueryHandler<GetProductsQuery, GetProductsDto>
 {
+    private IUnitOfWork UnitOfWork { get; }
+
+    public GetProductsQueryHandler(IUnitOfWork unitOfWork)
+    {
+        UnitOfWork = unitOfWork;
+    }
+
+    public async Task<Result<GetProductsDto>> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+    {
+        var result = await UnitOfWork.ProductRepository.GetAllProductsAsync(cancellationToken);
+        return new Result<GetProductsDto>(
+            new GetProductsDto(result.Select(prod => new GetProductDto(prod.Id, prod.Name))), true);
+    }
 }
